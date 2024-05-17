c65848a3d5a973705f4ce207209e980954e0301b
kdh5107 <bjmb12@naver.com>
2024-05-17 20:05:34 +0900

    짧은 수정

--- petmein/src/main/petmein/src/App.js
+++ petmein/src/main/petmein/src/App.js
@@ -1,16 +1,7 @@
 import "./App.css";
-import { useEffect, useState } from "react";
-import TopNav from "./nav/top-nav";
-import BottomNav from "./nav/bottom-nav";
 
 function App() {
-  return (
-    <div className="App">
-      <TopNav></TopNav>
-      <div>본문</div>
-      <BottomNav></BottomNav>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
